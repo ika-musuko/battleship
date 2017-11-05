34711e0ff9f1a6027c33e8c981caea948fef5b10
Sherwyn Sen <sherwyn.sen@gmail.com>
2017-11-05 15:51:44 -0800

    please read the explanations in SelfSpace and AttackSpace

--- AttackSpace.java
+++ AttackSpace.java
@@ -1,3 +1,7 @@
+// SelfSpace is an enum (like int) which represents a single space on attackBoard
+// there are two possible values: 
+//      AttackSpace.UNMARKED: the player has not tried to attack this square yet
+//      AttackSpace.MARKED: the player has already tried attacking this square
 public enum AttackSpace {
     UNMARKED,
     MARKED,
--- SelfSpace.java
+++ SelfSpace.java
@@ -1,3 +1,13 @@
+// SelfSpace is an enum (like int) which represents a single space on selfBoard
+// there are three possible values:
+//      SelfSpace.EMPTY = nothing here
+//      SelfSpace.SHIP  = a ship occupies this space (element in Player.selfGrid)
+//      SelfSpace.DESTROYED = part of ship which has been destroyed is in this space 
+//
+//      be sure to only reduce the total number of ships if and only if ALL spaces in a ship have been set to SelfSpace.DESTROYED!
+
+// set spaces with ships to SelfSpace.SHIP during setup mode
+// set to SelfSpace.DESTROYED if opponent attacks successfully 
 public enum SelfSpace {
     EMPTY,
     SHIP,
