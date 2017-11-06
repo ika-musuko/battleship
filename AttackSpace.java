a7566c3b150b45ab8c6c6ea39db8be9dc6c3afff
Sherwyn Sen <sherwyn.sen@gmail.com>
2017-11-05 17:24:12 -0800

    ANDREW read the comments in Player.java inside the attack() method

--- AttackSpace.java
+++ AttackSpace.java
@@ -2,6 +2,8 @@
 // there are two possible values: 
 //      AttackSpace.UNMARKED: the player has not tried to attack this square yet
 //      AttackSpace.MARKED: the player has already tried attacking this square
+//      AttackSpace.SUCCESS: this past attack was a success
+//      AttackSpace.MARKED: this past attack was a failure
 public enum AttackSpace {
     UNMARKED,
     MARKED,
